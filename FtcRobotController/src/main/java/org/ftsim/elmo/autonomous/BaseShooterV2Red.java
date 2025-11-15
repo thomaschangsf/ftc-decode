314653830f49d3ee1fa5eb544c45ffe367f67059
Mason <chang.masonc@gmail.com>
2025-11-14 19:36:39 -0800

    Mason's autonomous changes

---
+++ FtcRobotController/src/main/java/org/ftsim/elmo/autonomous/BaseShooterV2Red.java
@@ -0,0 +1,67 @@
+package org.ftsim.elmo;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="BaseShooterV1", group="Linear Opmode")
+public class BaseShooterV2Red extends LinearOpMode
+{
+    DcMotor motor1;
+    DcMotor motor2;
+    DcMotor motor3;
+    Servo leftShooterServo;
+    Servo rightShooterServo;
+
+    @Override
+    public void runOpMode() {
+
+        motor1 = hardwareMap.get(DcMotor.class,"motor1");
+        motor2 = hardwareMap.get(DcMotor.class,"motor2");
+        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        leftShooterServo = hardwareMap.get(Servo.class, "leftShooterServo");
+        rightShooterServo = hardwareMap.get(Servo.class, "rightShooterServo");
+        telemetry.addData("Status", "Initialized and Ready");
+        telemetry.update();
+
+        waitForStart();
+        leftShooterServo.setPosition(0);
+        rightShooterServo.setPosition(0);
+        motor3.setPower(0);
+        motor3.setPower(-0.46);
+        sleep(3000);
+        leftShooterServo.setPosition(1);
+        rightShooterServo.setPosition(-1);
+        sleep(1000);
+        leftShooterServo.setPosition(0);
+        rightShooterServo.setPosition(1);
+        sleep(1250);
+        leftShooterServo.setPosition(1);
+        rightShooterServo.setPosition(-1);
+        sleep(1000);
+        leftShooterServo.setPosition(0);
+        rightShooterServo.setPosition(1);
+        sleep(1250);
+        leftShooterServo.setPosition(1);
+        rightShooterServo.setPosition(-1);
+        sleep(1000);
+        leftShooterServo.setPosition(0);
+        rightShooterServo.setPosition(1);
+        motor3.setPower(0);
+        sleep(1500);
+        motor1.setPower(-1);
+        motor2.setPower(-1);
+        sleep(500);
+        motor2.setPower(1);
+        motor1.setPower(-1);
+        sleep(450);
+        motor2.setPower(-1);
+        motor1.setPower(-1);
+        sleep(650);
+        motor2.setPower(0);
+        motor1.setPower(0);
+        leftShooterServo.setPosition(0);
+        rightShooterServo.setPosition(0);
+    }
+}
