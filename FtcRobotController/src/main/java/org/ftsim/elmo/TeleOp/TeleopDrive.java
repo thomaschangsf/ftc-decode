fc5fd96a2445fd75b473a902ebd1b5f9f217b512
Mason <chang.masonc@gmail.com>
2025-11-17 19:17:56 -0800

    Shooting Power based on Distance from Goal; Mason

--- FtcRobotController/src/main/java/org/ftsim/elmo/TeleOp/TeleopDrive.java
+++ FtcRobotController/src/main/java/org/ftsim/elmo/TeleOp/TeleopDrive.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="motor1")
-public class TotalTeleOpV0 extends OpMode
+public class TeleOpDrive extends OpMode
 {
     DcMotor motor1;
     DcMotor motor2;
