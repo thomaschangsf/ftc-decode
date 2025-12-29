dcd1139bac52e73cce463f4bf07e9a885b10b769
Mason <chang.masonc@gmail.com>
2025-12-28 21:36:22 -0800

    COUNTERSTEERING

--- FtcRobotController/src/main/java/org/ftsim/elmo/TeleOp/TotalTeleOpv23.java
+++ FtcRobotController/src/main/java/org/ftsim/elmo/TeleOp/TotalTeleOpv23.java
@@ -55,7 +55,15 @@ public class TotalTeleOpv23 extends LinearOpMode {
             }
             motorrf.setPower(motorrfPower);
             
-            double motorlbPower = Range.clip(motorrbPower - MOTORLB_POWER_REDUCTION, -maxPower, maxPower);
+            // motorlb has MOTORLB_POWER_REDUCTION less power, but only if stick is being touched
+            double motorlbPower;
+            if (Math.abs(drive) > 0.01 || Math.abs(turn) > 0.01) {
+                // Stick is being used: apply power reduction
+                motorlbPower = Range.clip(motorrbPower - MOTORLB_POWER_REDUCTION, -maxPower, maxPower);
+            } else {
+                // Stick not being touched: same power as right side
+                motorlbPower = motorrbPower;
+            }
             motorlb.setPower(motorlbPower);
             double motorlfPower = Range.clip(drive + turn, -maxPower, maxPower);
             motorlf.setPower(-motorlfPower);
