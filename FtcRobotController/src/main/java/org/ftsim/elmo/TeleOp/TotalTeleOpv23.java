b006256c194ba1aaaaf979bcbe95221c9b256e7b
Mason <chang.masonc@gmail.com>
2026-01-11 13:36:46 -0800

    Competition Changes

--- FtcRobotController/src/main/java/org/ftsim/elmo/TeleOp/TotalTeleOpv23.java
+++ FtcRobotController/src/main/java/org/ftsim/elmo/TeleOp/TotalTeleOpv23.java
@@ -49,8 +49,7 @@ public class TotalTeleOpv23 extends LinearOpMode {
             double motorrfPower;
             if (Math.abs(drive) > 0.01) {
                 motorrfPower = Range.clip(motorrbPower + (motorrbPower >= 0 ? MOTORRF_POWER_BOOST : -MOTORRF_POWER_BOOST), -maxPower, maxPower);
-            }
-            else {
+            } else {
                 motorrfPower = motorrbPower;
             }
             motorrf.setPower(motorrfPower);
@@ -58,8 +57,7 @@ public class TotalTeleOpv23 extends LinearOpMode {
             double motorlbPower;
             if (Math.abs(turn) > 0.01) {
                 motorlbPower = Range.clip(drive + turn, -maxPower, maxPower);
-            } 
-            else if (Math.abs(drive) > 0.01 && Math.abs(motorrbPower) > 0.5) {
+            } else if (Math.abs(drive) > 0.01 && Math.abs(motorrbPower) > 0.5) {
                 motorlbPower = Range.clip(motorrbPower - MOTORLB_POWER_REDUCTION, -maxPower, maxPower);
             } else {
                 motorlbPower = motorrbPower;
@@ -69,21 +67,19 @@ public class TotalTeleOpv23 extends LinearOpMode {
             double motorlfPower = Range.clip(drive + turn, -maxPower, maxPower);
             motorlf.setPower(-motorlfPower);
 
-            if(gamepad1.right_bumper){
+            if (gamepad1.right_bumper) {
                 motorlb.setPower(-0.5);
                 motorrf.setPower(-0.8);
                 motorrb.setPower(0.8);
                 motorlf.setPower(-0.5);
             }
 
-            if(gamepad1.left_bumper){
+            if (gamepad1.left_bumper) {
                 motorlb.setPower(0.5);
                 motorrf.setPower(0.8);
                 motorrb.setPower(-0.8);
                 motorlf.setPower(0.5);
-            }
-
-            else{
+            } else {
                 motorlb.setPower(0);
                 motorrf.setPower(0);
                 motorrb.setPower(0);
@@ -92,67 +88,44 @@ public class TotalTeleOpv23 extends LinearOpMode {
 
             //orignal loop time 9 secs
             if (gamepad1.right_trigger > 0.1) { //Shooting loop
-             //BALL 1 1250 m;
-             motorfw.setPower(-1);
-             sleep(200);
-             motorfw.setPower(-0.68);
-             sleep(900);
-             leftShooterServo.setPosition(1); //open to shoot
-             rightShooterServo.setPosition(0);
-             sleep(350); //gives ball time to escape servo
-             leftShooterServo.setPosition(0);//close for next shot
-             rightShooterServo.setPosition(1);
+                leftShooterServo.setPosition(0);
+                rightShooterServo.setPosition(1);
+                motorfw.setPower(0);
 
-            // BALL 2 1025ms
-            motorfw.setPower(-0.67); //12.6 volts
-            sleep(800);
-            leftShooterServo.setPosition(1); //open to shoot
-            rightShooterServo.setPosition(0);
-            sleep(300); //gives ball time to escape servo
-            leftShooterServo.setPosition(0);//close for next shot
-            rightShooterServo.setPosition(1);
+                //BALL 1 1250 m;
+                motorfw.setPower(-1);
+                sleep(300);
+                motorfw.setPower(-0.62);
+                sleep(900);
+                leftShooterServo.setPosition(1); //open to shoot
+                rightShooterServo.setPosition(0);
+                sleep(325); //gives ball time to escape servo
+                leftShooterServo.setPosition(0);//close for next shot
+                rightShooterServo.setPosition(1);
 
-            // BALL 3 1325 ms
-            motorfw.setPower(-0.67); //12.6 volts
-            sleep(675);
-            leftShooterServo.setPosition(1); //open to shoot
-            rightShooterServo.setPosition(0);
-            sleep(325); //gives ball time to escape servo
-            leftShooterServo.setPosition(0);//close for next shot
-            rightShooterServo.setPosition(1);
+                // BALL 2 1025ms
+                motorfw.setPower(-0.62); //12.6 volts
+                sleep(850);
+                leftShooterServo.setPosition(1); //open to shoot
+                rightShooterServo.setPosition(0);
+                sleep(300); //gives ball time to escape servo
+                leftShooterServo.setPosition(0);//close for next shot
+                rightShooterServo.setPosition(1);
 
-            sleep(1200);
-            motorfw.setPower(0);
-            motorfw.setPower(-1);
-            sleep(100);
-            //To much power, shoots to high, to little, shoots to horizontal
+                // BALL 3 1325 ms
+                motorfw.setPower(-0.62); //12.6 volts
+                sleep(850);
+                leftShooterServo.setPosition(1); //open to shoot
+                rightShooterServo.setPosition(0);
+                sleep(325); //gives ball time to escape servo
+                leftShooterServo.setPosition(0);//close for next shot
+                rightShooterServo.setPosition(1);
 
-
-            // give ball to fly and flywhell to regain speed
-            //sleep(400);
-            //sleep(300 + i*100);
-            // Too low. 200,300 --> collide or no power
-
-            //}
-
-            //}
-
-            //if(gamepad.left_trigger > 0.1){
-            // BALL 1 1400 m;
-            //motorfw.setPower(-1);
-            //sleep(200);
-            //motorfw.setPower(-0.5);
-            //sleep(900);
-            //leftShooterServo.setPosition(1); //open to shoot
-            //rightShooterServo.setPosition(0);
-            //sleep(350); //gives ball time to escape servo
-            //leftShooterServo.setPosition(0);//close for next shot
-            //rightShooterServo.setPosition(1);
-            //}
-
-            // Update telemetry to display current status
-            telemetry.update();
+                sleep(1500);
+                motorfw.setPower(1);
+                sleep(450);
+                motorfw.setPower(0);
+            }
         }
     }
 }
-
